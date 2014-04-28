46e4803d0f26d1da14ca9e8ae707c2fbe0c0331f
Raj Raj <neeraj2608@gmail.com>
2014-04-28 00:12:42 -0400

    Added newline and space to allowed plain text chars.

--- src/cipher/Encr.hs
+++ src/cipher/Encr.hs
@@ -3,41 +3,41 @@ module Encr where
 import EncrUtilities
 
 encipher :: Int -> String -> String -> String
-encipher blockSize key = cipher blockSize (symFromBlock key)
+encipher bSize key = cipher bSize (symFromBlock key)
 
 decipher :: Int -> String -> String -> String
-decipher blockSize key = cipher blockSize (- symFromBlock key)
+decipher bSize key = cipher bSize (- symFromBlock key)
 
 cipher :: Int -> Integer -> String -> String
-cipher blockSize keySymbol = concat .
-                             map blockFromSym . -- convert back to blocks with plain-alphabet symbols
-                             map shiftSymbol . -- encipher
-                             map symFromBlock . -- convert to cipher-alphabet symbols
-                             fixedSizeBlocks blockSize ' '
-                             where
-                               shiftSymbol x = (x + keySymbol) `mod` cipherAlphabetSize
-                               cipherAlphabetSize = numeralBase ^ blockSize
+cipher bSize keySymbol = concat .
+                         map blockFromSym . -- convert back to blocks with plain-alphabet symbols
+                         map shiftSymbol . -- encipher
+                         map symFromBlock . -- convert to cipher-alphabet symbols
+                         fixedSizeBlocks bSize ' '
+                         where
+                           shiftSymbol x = (x + keySymbol) `mod` cipherAlphabetSize
+                           cipherAlphabetSize = numeralBase ^ bSize
 
 -- pads blocks with padChar if the block size is smaller than blockSize
 fixedSizeBlocks :: Int -> Char -> String -> [String]
 fixedSizeBlocks _ _ [] = []
-fixedSizeBlocks bSize padChar x | bSize > (length x) = [x ++ (replicate (bSize - (length x)) padChar)]
-                                | otherwise = [take bSize x] ++ fixedSizeBlocks bSize padChar (drop bSize x)
+fixedSizeBlocks bSize padChar x | bSize > length x = [x ++ replicate (bSize - length x) padChar]
+                                | otherwise = take bSize x : fixedSizeBlocks bSize padChar (drop bSize x)
 
-maximDijkstra, ciphertext, plaintext :: String
+maximDijkstra :: String
 maximDijkstra = "Besides a mathematical inclination, an exceptionally\n" ++
                 "good mastery of one's native tongue is the most vital\n" ++
                 "asset of a competent programmer.\n"
 
-ciphertext = encipher blockSize key maximDijkstra
-plaintext = decipher blockSize key ciphertext
+k :: String
+k = "computing science"
+
+blockSize :: Int
+blockSize = 10
 
 main :: IO ()
 main = do
-         putStrLn ciphertext
-         putStrLn plaintext
-
-key :: String
-key = "computing science"
-blockSize :: Int
-blockSize = 10
+         let ciphertext = encipher blockSize k maximDijkstra
+         let plaintext = decipher blockSize k ciphertext
+         putStrLn $ "Cipher text: " ++ ciphertext
+         putStr $ "Plain text: " ++ plaintext
--- src/cipher/EncrUtilities.hs
+++ src/cipher/EncrUtilities.hs
@@ -20,16 +20,20 @@ numeralsFromInteger b x = numeralsFromInteger b d ++ [r]
                                 (d, r) = myDiv x
 
 integerCodeFromChar :: Char -> Integer
-integerCodeFromChar x | fromEnum x > maxCode || fromEnum x < minCode = error "Char out of allowed range."
+integerCodeFromChar x | x == ' ' = fromIntegral (minCode-1)
+                      | x == '\n' = fromIntegral (minCode-2)
+                      | fromEnum x > maxCode || fromEnum x < minCode = error "Only printable ASCII chars, space and newline allowed."
                       | otherwise = fromIntegral (fromEnum x - minCode)
 
 charFromIntegerCode :: Integer -> Char
-charFromIntegerCode x = toEnum (fromIntegral x + minCode)
-
-numeralBase :: Integer
-numeralBase = fromIntegral(maxCode - minCode + 1 + numExtraCodes)
+charFromIntegerCode x | x == fromIntegral minCode-1 = ' '
+                      | x == fromIntegral minCode-2 = '\n'
+                      | otherwise = toEnum (fromIntegral x + minCode)
 
 maxCode, minCode, numExtraCodes :: Int
 maxCode = 126 -- set of code-characters =
 minCode = 32 -- {tab, newline, toEnum 32 ... toEnum 126}
 numExtraCodes = 2
+
+numeralBase :: Integer
+numeralBase = fromIntegral(maxCode - minCode + 1 + numExtraCodes)
